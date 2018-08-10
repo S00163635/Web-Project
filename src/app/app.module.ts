0e6af5f42227cda60a64db684a97235b9cd6ce90
Emily Arnold <S00163635@mail.itsligo.ie>
2018-08-09 22:00:15 -0600

    adding first components and uploading to github

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { MoviesComponent } from './movies/movies.component';
+import { AboutComponent } from './about/about.component';
+import { UserComponent } from './user/user.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    MoviesComponent,
+    AboutComponent,
+    UserComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule
